43f1744657aa4c40aa2f8ec9dfe09dc7c75b9dcc
Justin <justin.ardini@gmail.com>
2011-02-12 03:58:55 -0500

    reduced server message push time

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ startGame = function() {
 		clients[i].playerId = i;
 		clients[i].entities = {};
 	}
-	setInterval(pushUpdates, 1000 / 500);
+	setInterval(pushUpdates, 1000 / 100);
 }
 
 // Push updates to all clients
