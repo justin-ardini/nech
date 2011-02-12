65e07a07a4f1f1c7ac40eddc1afe5a0168d7df60
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-02-12 01:54:28 -0500

    playercontroller clamps player to screen

--- src/playercontroller.js
+++ src/playercontroller.js
@@ -13,18 +13,12 @@ function PlayerController(game, entity) {
 
 PlayerController.prototype.tick = function(seconds) {
 	var speed = 300;
-	if (this.upKey) {
-		this.entity.position.y -= speed * seconds;
-	}
-	if (this.downKey) {
-		this.entity.position.y += speed * seconds;
-	}
-	if (this.leftKey) {
-		this.entity.position.x -= speed * seconds;
-	}
-	if (this.rightKey) {
-		this.entity.position.x += speed * seconds;
-	}
+	if (this.upKey) this.entity.position.y -= speed * seconds;
+	if (this.downKey) this.entity.position.y += speed * seconds;
+	if (this.leftKey) this.entity.position.x -= speed * seconds;
+	if (this.rightKey) this.entity.position.x += speed * seconds;
+	this.entity.position.x = Math.max(this.entity.radius, Math.min(GAME_WIDTH - this.entity.radius, this.entity.position.x));
+	this.entity.position.y = Math.max(this.entity.radius, Math.min(GAME_HEIGHT - this.entity.radius, this.entity.position.y));
 
 	this.fireDelay -= seconds;
 	if (this.fireDelay <= 0 && this.shootKey) {
