53809ae0250ab02de2a25736c1ba6e6fa2861495
Justin <justin.ardini@gmail.com>
2011-02-12 03:53:18 -0500

    added server check for undefined clients when receiving messages

--- server.js
+++ server.js
@@ -77,7 +77,9 @@ socket.on('connection', function(client) {
 		console.log('Received message from client ' + this.playerId);
 		if (message.entities.length > 0) {
 			var playerId = message.entities[0].playerId;
-			clients[playerId].entities = message.entities;
+			if (clients[playerId] !== undefined) {
+				clients[playerId].entities = message.entities;
+			}
 		}
 	});
 
