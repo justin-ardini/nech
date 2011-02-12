177e37e647e8cac38326ead86571cf899bac6167
Evan Wallace <evan@evan-wallaces-macbook-pro.local>
2011-02-12 02:59:56 -0500

    fixes for other direction

--- src/test_enemy.js
+++ src/test_enemy.js
@@ -2,19 +2,7 @@ __extends__(TestEnemy, Entity);
 
 function TestEnemy(position) {
 	Entity.prototype.constructor.call(this, position, 90, 2);
-	this.particleTimer = 0;
 }
-
-TestEnemy.prototype.tick = function(seconds) {
-	this.position.x -= seconds * 10;
-
-	this.particleTimer += seconds;
-	while (this.particleTimer > 0) {
-		this.particleTimer -= 0.025;
-		var vel = new Vector(100 + 50 * Math.random(), 20 * Math.random() - 10);
-		Particle().position(this.position).velocity(vel).circle().radius(5).expand(0.2);
-	}
-};
 
 TestEnemy.prototype.drawImpl = function(c) {
 	c.beginPath();
